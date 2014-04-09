a5e0e08ceb815280fb3d869f225c9fba63196501
s <s@debian.NETCOMM>
2014-04-10 08:52:52 +1200

    oh right yeah it's that thing put it back in Form and you've got something that'll grow

---
+++ xest.pl
@@ -0,0 +1,167 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+# the fact that THIS is the only thing recommended to be in here is beautiful
+# and it's not even a tomb: it's really a Texty producer
+# and through there somewhere is stylehouse
+use Scriptalicious;
+# last THAT joke I promise
+# I didn't even notice there was no depth
+# music's about feeling good anyway because
+
+# sub get_goya { # tried it just named goya
+# only Hostinfo can truly get goya because it's the one place that's finally one thing
+
+# it's always ripples we're feeling through something
+
+my $goya =
+    [ map {
+        { top => $_ },
+    } 1..20 ];
+
+my $order_in_space;
+my $space;
+my $junk; # trash guy in the background - I'm throwing away I wheel I couldn't get replaced for my hand truck for gear fuck it all
+my $stuff;
+my $amp = 1.2;
+my $q = 0.5;
+# it's not a hash, it doesn't get along with the hash people still # now it does
+# looks more like 9 tabs than a hash
+# exactly my content, adjusted for everything
+# man Arabia is beautiful
+# it distills lush people
+my $changes = {}; # ch ch ch
+
+sub order_in_space {
+    for my $angle (qw{top}) {
+        # my $vast = $goya->{$angle}; # Karl Sagan
+
+        $order_in_space->{$angle} = { max => 0, min => undef};
+        for my $span (@$goya) {
+
+            if (!defined($order_in_space->{$angle}->{min})
+                || $order_in_space->{$angle}->{min} > $span->{$angle}) {
+                $order_in_space->{$angle}->{min} = $span->{$angle};
+            }
+            if ($order_in_space->{$angle}->{max} < $span->{$angle}) {
+                $order_in_space->{$angle}->{max} = $span->{$angle};
+            }
+        }
+    }
+}
+
+sub space { # figure out the whole space
+    # the rest of this is up to innovation, finally
+    $space = {
+        top => {},
+#        left => {},
+#        right => {}, # right now is lost because it's all really coming from the center
+    };
+    # wow dinosaurs died for us so we could be safe and use their residue as an energy source to get us here
+    # we can fit the essential DNA in a twitter message that shows people what the author wanted to express
+    # and gives them expression tools of their own
+    for my $g (@$stuff) { # everything is a minor God
+        my $span = $space->{span}; # [{top => etc}s]
+        for my $angle (qw{ top }) { # when you give it proper space from the top
+            my $value = $span->{$angle}; # and then you tidily fix the flaws in the rest of it
+            my $where = $junk->{$angle}->{$value} ||= []; # cymbal rush - it's just where all that jargon hangs out naturally
+            push @$where, $span; # cymbal rush
+        }
+    }
+    # I don't want to be persued as an individual
+    # music makes that clear
+    # so the main thing to learn if you haven't figured out babylon is what else is there to learn.
+    # people art on the ground
+}
+
+sub angle {
+    for my $angle (qw{left top right}) {
+        my $dimension = $space->{$angle}; # Karl Sagan
+
+        $order_in_space->{$angle} = { max => 0, min => undef };
+        for my $span ($vast) {
+            for my $end (qw{min max}) {
+                if (!defined($order_in_space->{$angle}->{min})
+                    || $order_in_space->{$angle}->{min} > $span->{$angle}) {
+                    $order_in_space->{$angle}->{min} = $span->{$angle};
+                }
+            }
+            if ($order_in_space->{$angle}->{max} < $span->{$angle}) {
+                $order_in_space->{$angle}->{max} = $span->{$angle};
+            }
+        }
+    }
+}
+
+# good experiences spiral out of control
+# we can build all the different aspects along a spiral
+# but only because we're working with ourselves through time as far as we know
+# so they still overlap enough to create a sane work environment
+# my beards got curves it's a girl, something that grows related to everything
+# LoL! $&$
+# everything that exists on the screen is part of the experience
+# the useful way our brains work is injecting space into ideas, or just being
+# tomb for the underworld and its architecture
+# it's not even really this but you have to get it past here first
+sub inject_space {
+    my $self = shift;
+
+    # get_goya(); # it's almost never a concept you get to play with on its own
+    
+    order_in_space();
+
+#    say anydump( $order_in_space ); # this one is chief of madness, somewhere around here
+# this would be easier on mescalin or something really travelly...
+    space(); # any of these things can destroy the resonant life frequency we require
+# so now things are in the $junk because space is a lie
+
+    say anydump( $junk ); # this one is chief of madness, somewhere around here
+    my $sf; # this was wrong but it existed somewhere important anyway $_->{min} - $_->{max}
+
+    my $angle; # you can have it all the way you just need to take this thing now
+# ooh yeah it's so crazy. we taste angles.
+
+    my $ord = $order_in_space;
+    # I can show you the map that $synthspace produces
+    my $synthspace = sub {
+        my $angle = shift;
+        my $span = shift;
+        my $min = $ord->{$angle}->{min};
+        my $max = $ord->{$angle}->{max};
+        my $half = $max / 2; # sharing is caring
+        # you could fall into babylon any time
+        # it's what we call mental health now
+        # but it's right around here somewhere...
+
+        my $gone = $span->{$angle} - $min;
+        my $way = $gone / $max; # way is the tiny thing
+        my $qfactor = $q;
+        $qfactor = -$q if $way < $half;
+        my $x = $qfactor * $way * $q;
+        $way += $x;
+        print "\t$way";
+
+        $span->{"old_$angle"} = $span->{$angle};
+        $span->{$angle} += $way;
+        $changes->{$span->id}->{$angle} = $way;
+    };
+
+    for $angle (qw{left top right}) {
+        $changes->{$angle} ||= [];
+        for my $points ($order_in_space->{$angle}) {
+            # don't go looking for the points
+            for my $point (values %$points) { # we are almost nothing with an insane society
+                if (my $a_space = $space->{$angle}->{$point}) { # but there's something here
+                    for my $span ($stuff) {
+                        $synthspace->($angle => $span);
+                    }
+                }
+            }
+        }
+    }
+
+    return $stuff;
+}
+
+say join"\n",grep !/\d+/, split "\n", anydump(inject_space());
+
