949023090f722da2d18685cd5e34b373f6ae92ff
s <s@debian.NETCOMM>
2014-06-01 18:42:45 +1200

    lets go sniff glue with codemirror

--- lib/Codo.pm
+++ lib/Codo.pm
@@ -49,7 +49,7 @@ sub new {
     
 
     my $Codo =
-    $hi->create_view($self, Codo => "width:58%;  background: #f5f0f5; color: #afc; border: 2px solid light-blue;");
+    $hi->create_view($self, Codo => "width:58%;  background: #f5f0f5; color: #afc; border: 2px solid light-blue; position: fixed; right: 0px;");
 
         $Codo->spawn_ceiling($self, codolist => "width:98%;  background: #402a35; color: #afc; height: 60px;");
 
@@ -73,7 +73,6 @@ sub new {
 
     return $self;
 }
-
 sub menu {
     my $self = shift;
     $self->{menu} ||= {
