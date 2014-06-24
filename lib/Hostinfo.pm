6bdbcd296385899838fabeee3dc0891fc058ab4a
styleshed <s@debian.NETCOMM>
2014-06-24 23:26:40 +1200

    trippy

--- lib/Hostinfo.pm
+++ lib/Hostinfo.pm
@@ -31,7 +31,7 @@ sub init_flood {
         "width: 100%; height: 100%; background: #A65300; overflow: none;position: absolute; top: $self->{horizon}; left: 0px; z-index:-1;"
     );
     my $f = $self->{ground}->spawn_floozy($self, "flood",
-        "width: 42%; min-width:509.188px; background: #337921; height: 100%; overflow: scroll;position: absolute; right:0px;z-index:-1;"
+        "width: 42%; min-width:509.188px; background: #337921; height: 100%; overflow: scroll;position: absolute; background-image: url(blue-velvet-sofa.jpg); right:0px;z-index:-1;"
     );
     my $fm = $f->spawn_ceiling(
         "flood_ceiling",
