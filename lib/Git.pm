906def69eedc736fc3eb49f18d4a58d21ba27fde
styleshed <nostrasteve@gmail.com>
2014-07-09 19:59:50 +1200

    don run

--- lib/Git.pm
+++ lib/Git.pm
@@ -89,7 +89,7 @@ sub gitrack {
     },
     '⏚' => sub {
         say" HEading to $r";
-        run('cd ../'.$r.' && git gui');
+        $self->spawn_proc('cd ../'.$r.' && git gui');
     },
     'ܤ' => sub {
         $self->spawn_proc('cd ../'.$r.' && touch stylehouse.pl');
