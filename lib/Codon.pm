c9b93822ac4d0c22e94649ba830cd5e0304a2fb9
styleshed <nostrasteve@gmail.com>
2014-07-12 00:19:16 +1200

    looks full of space for code

--- lib/Codon.pm
+++ lib/Codon.pm
@@ -191,7 +191,8 @@ CM
             );
         }
     }
-    $H->JS("\$('#CodeMirror').css('width', '90%');");
+    $H->JS("\$('.CodeMirror').css('width', '100%');");
+    $H->JS("\$('.CodeMirror').parent().css('width', '100%');");
 
     $temp->wipehtml() if $temp;
 
