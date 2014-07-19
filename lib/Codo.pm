01699e7221c5f0f01144db1bc3f4d16eb3a004eb
styleshed <nostrasteve@gmail.com>
2014-07-19 18:03:31 +1200

    tid

--- lib/Codo.pm
+++ lib/Codo.pm
@@ -384,7 +384,7 @@ sub writefile {
     $self->{hostinfo}->spurt("newcode", $stuff);
     my $diff = `diff newcode $filename`;
     $self->{hostinfo}->Info($diff);
-    `mv newcode $filename` if $diff;
+    $diff ? `mv newcode $filename` : `rm newcode`;
 }
 sub random_colour_background {
     my ($rgb) = join", ", map int rand 255, 1 .. 3;
