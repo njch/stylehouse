b5e497ac25e71a3c95e625166b3c26273bb23313
styleshed <nostrasteve@gmail.com>
2014-08-03 21:13:05 +1200

    wa

--- lib/View.pm
+++ lib/View.pm
@@ -287,8 +287,7 @@ sub takeover {
     }
     elsif (my $apid = $texty->{hooks}->{append}) {
         if ($apid =~ /\w\w/) {
-            $self->append_spans("#$apid", $html, "parent().after");
-            $self->{hostinfo}->JS("\$('#$apid').parent().slideUp(200, function () { \$(this).remove() });");
+            $self->append_spans("#$apid", $html, "after");
             $self->{hostinfo}->JS("\$('.$texty->{id}').slideUp(0).slideDown(500);");
             return;
         }
