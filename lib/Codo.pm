8ca7798541482bb5b82376770f4e076850083ba6
styleshed <nostrasteve@gmail.com>
2014-08-06 18:34:48 +1200

    jumps

--- lib/Codo.pm
+++ lib/Codo.pm
@@ -45,7 +45,7 @@ sub new {
     die "No code dir: $cd" unless -d $cd;
     $self->{code_dir} = $cd;
 
-    my $codoback = "#b60"; #"001452"
+    my $codoback = "#B2B247"; #"001452"
     $codoback = "rgba(150,78,50,0.85)" if $ss eq "shed";
     
     my $Codo =
