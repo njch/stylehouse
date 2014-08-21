9435f4cb319ae78a4a3ce9b29421f55cf1047370
styleshed <nostrasteve@gmail.com>
2014-08-21 15:26:27 +1200

    harmless weirdness

--- lib/Ghost.pm
+++ lib/Ghost.pm
@@ -431,7 +431,9 @@ sub haunt { # arrives through here
         $o->{B}->{Lo} = $L; # L heading back out
     }
     #TODO
-    $i->{B}->{Li} && $H->Say("reit", $i,ki($i)) && die "reiterate you?" unless $G->way_was("revisit");
+    if ($i->{B}->{Li} && !$G->way_was("revisit")) {
+        $G->Flab("Funny, end of haunt, way in already had i->B->Li", $i->pint, $i, $L);
+    }
     $i->{B}->{Li} = $L; # L heading in
 
     return ($L, $G->{o});
