4998834249265cf8c6dc06f12b8b3dac364e66b2
styleshed <nostrasteve@gmail.com>
2014-08-23 16:55:53 +1200

    ye

--- lib/Ghost.pm
+++ lib/Ghost.pm
@@ -103,7 +103,7 @@ sub ghostlyprinty {
     $gp_inarow--;
     join "  ", @s
 }
-sub gpty { ghostlyprinty(@_) };
+sub gpty { ghostlyprinty('NOHTML',@_) };
 sub Flab {
     my $G = shift;
     ref $G eq "Ghost" || die "send Ghost";
