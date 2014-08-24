aa56489b6b142878590e65b3b8f7265fbf444225
styleshed <nostrasteve@gmail.com>
2014-08-25 10:33:27 +1200

    moves, drops

--- killbo.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl
-use Scriptalicious;
-my $ps = join"", grep /morbo/, `ps faux `;
-$ps =~ /s\s+(\d+) +/ || barf "nah cannot find";
-`kill -KILL $1`;
-
--- rmproc.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
-`cat /dev/null > proc/start`;
-`cat /dev/null > proc/list`;
-`rm proc/*.*` if glob('proc/*.*');
