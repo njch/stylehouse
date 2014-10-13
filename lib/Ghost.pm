d69bf1b9f8fa015eb63fcec9cae1322ba6ed742a
styleshed <nostrasteve@gmail.com>
2014-10-13 14:16:20 +1300

    rhythm jumps in
    Signed-off-by: styleshed <nostrasteve@gmail.com>

--- lib/Ghost.pm
+++ lib/Ghost.pm
@@ -44,6 +44,7 @@ sub new {
     $self->{T} = shift;
     
     my $name = $self->{T}->{name};
+    $self->W || die "no Wormhole>?";
     
     $self->{O} = $self->{T}->{O};
     $self->{GGs} = [];
@@ -59,7 +60,7 @@ sub new {
         say " . . guess way is $guess";
         @ways = $guess;
     };
-    my $way = join", ",@ways;
+    my $way = join ", ", @ways;
     $name = "$name`($way)";
     $self->{name} = $name;
     $self->{way} = $way;
@@ -346,8 +347,7 @@ sub Gc { # TODO merge into ^
 }
 sub W {
     my $self = shift;
-    return $self->{W} if $self->{W};
-    $self->{T}->{W} ||= Wormhole->new($H->intro, $self, "wormholes/$self->{name}/0");
+    $self->{T}->{W} = $self->{W} ||= Wormhole->new($H->intro, $self, "wormholes/$self->{name}/0");
 }
 sub RW {
     my $self = shift;
