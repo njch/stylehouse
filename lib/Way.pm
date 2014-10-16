7e92378ca05c598c1b8ccdceb51f715a8f8aa87b
styleshed <nostrasteve@gmail.com>
2014-10-17 08:50:08 +1300

    In-ject Clange from GG: into spawned G

--- lib/Way.pm
+++ lib/Way.pm
@@ -34,7 +34,7 @@ sub from {
     my $w = shift;
     my $s = shift;
     for my $k (keys %$s) {
-        next if $k eq "uuid" || $k eq "G"; # or Li or Lo or who knows what # SCHEMAS
+        next if $k eq "uuid" || $k eq "G"; # or Li or Lo or who knows what # SCHEMAS # MERGINESS
         my $t = $w->{$k};
         my $f = $s->{$k};
         
