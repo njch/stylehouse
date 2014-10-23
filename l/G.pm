38381fa60d2e7fd8fc5e1555c2347983a613c46d
styleshed <nostrasteve@gmail.com>
2014-10-23 15:48:23 +1300

    what now...

---
+++ l/G.pm
@@ -0,0 +1,31 @@
+
+package G;
+use strict;
+use utf8;
+use base 'Ghost';
+
+sub new {
+    my $G = shift;
+    my $G = bless {}, shift;
+    shift->($G); delete $self->{hostinfo};
+    
+    my ($name, $O, @ways) = @_;
+    
+    $G->{GGs} = []
+    $G->{O} = $O; # TODO A
+    
+    $G->W || die "no Wormhole?";
+    
+    my $way = join ", ", @ways;
+    $G->{name} = "$name`($way)";
+    $G->{way} = $way;
+    say "Ghost named $G->{name}";
+    
+    $G->load_ways(@ways);
+    
+    push @{$G->{O}->{GGs}}, $G if ref $G->{O} =~ /^G/;
+    
+    return $self;
+}
+
+1;
