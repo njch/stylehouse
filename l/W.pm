70e095fa3486165370115b6b5119cd6987424e6a
styleshed <nostrasteve@gmail.com>
2014-10-25 19:38:23 +1300

    Hmm

--- l/W.pm
+++ l/W.pm
@@ -5,9 +5,14 @@ use warnings;
 use utf8;
 use lib 'lib';
 use feature 'say';
+use base 'Wormhole';
+
 sub new {
     my $W = shift;
     $W->{name} = "$W->{A}->{u}->{i}->{name}"; # TODO A tract mixinout naming
+    $W->{G} = shift;
+    $W->{n} = 0;
+    $W->wormfile_load(shift);
     
     $W
 }
