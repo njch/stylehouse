e4ff687fa44055a7fff8b5dbb8f965f5504771b7
styleshed <nostrasteve@gmail.com>
2014-10-25 18:45:48 +1300

    racing

--- l/W.pm
+++ l/W.pm
@@ -7,7 +7,7 @@ use lib 'lib';
 use feature 'say';
 sub new {
     my $W = shift;
-    $W->{name} = "$W->{A}->{u}->{i}->{name}"; # A tract mixinout naming
+    $W->{name} = "$W->{A}->{u}->{i}->{name}"; # TODO A tract mixinout naming
     
     $W
 }
