d74020f67a9c3fb995c4e12eaef2c9a05cd8de1f
styleshed <nostrasteve@gmail.com>
2015-01-05 10:00:05 +1300

    gone

--- l/G.pm
+++ l/G.pm
@@ -188,11 +188,6 @@ sub TRub {
     my $G = shift;
     my $a = {};
     my $R = $G->{R} ||= $G->{A}->spawn("R");
-    # like du, vectors space out after $k
-    #   for increment spacing over the braid
-
-    # nah just chuck stuff in $a
-    # everything should have more meaning than that
     my @a = @_;
     while (@a) {
         my ($k, $v) = (shift(@a), shift(@a));
@@ -238,12 +233,6 @@ sub Gf {
 
 sub HGf {
     $H->{G}->Gf(@_)
-}
-
-sub W {
-    my $G = shift;
-    die "Hi";
-    $G->{W}
 }
 
 sub parse_babble {
@@ -334,125 +323,6 @@ sub parse_babble {
 
 
     $eval;
-}
-
-sub du {
-    my $G = shift;
-    my $a = shift;
-    # how to get around the Objs' data
-    my $s = $a->{s} ||= $G->dus();
-    my $i = $a->{i};
-    my $n = $a->{n};
-    $a->{e} = 2 if !defined $a->{e};
-
-    my $c = {};
-    $a->{as} ||= [];
-    die sw($a) if !defined $a->{i};
-    push @{$a->{as}}, $a;
-    $a->{ds} = [@{$a->{ds}||[]}, $a];
-
-    return {} if @{$a->{ds}} > 12 || 2 < grep {ref $_->{i} && $_->{i} eq $i} @{$a->{as}};
-
-    my $ref = ref $i;
-    my $is = $s->{$ref} || $s->{default};
-    $is ||= $s->{HASH} if "$i" =~ /^\w+=HASH\(/;
-    $is ||= $s->{default} || return {};
-
-    # $mustb
-    # does HASH key importance if 0>e<1
-    # travels every thing      if e>=1
-    #                also      if 0.5<e<1
-    # since it must want to know links by then
-    # overthinking must be reached somehow
-    # going over a line
-    # at 5
-    # how much is happened
-    # sw is a channel
-    # the way in the splat...
-
-    # there's some more flipping around: TODO
-    # an e mod possibly coming from mustb (missing part)
-    # so some mustb key can e above 0.5 and see travel
-
-    # could return somehow weighted graph, hmm
-    # oldworld really doesn't like that idea
-    # maybe if du was grabbed as more abstract chunks with A action
-    # driving the new codons basically...
-
-    # snapping branches off the concept of ^ for now
-
-    # we sculpt data as fractions of energy that enlightens the 1-9 meanings
-    # fuck binary
-
-    # $an->{s}, etc can be modded as meaning builds down
-    # but only for 
-    # (separado until much future brings everything together)
-
-    my $mustb = { map { $_ => 1 } split ',', $is->{mustb} } if $is->{mustb};
-
-    for my $j ($is->{it}->($i)) {
-        my $k = delete $j->{k};
-        my $K = delete $j->{K};
-        my $v = delete $j->{v};
-
-        $j = {%$is, %$j};
-        my $an = {%$a, i => $v};
-
-        my $ohms = defined $j->{oh} ? $j->{oh} : 1;
-
-        $an->{e} -= $ohms;
-
-        my $rk = "$k $an->{e}";
-        $c->{$rk} = $v;
-
-        say join("", ("  ") x scalar(@{$a->{ds}}))
-            ." $a->{e} - $ohms  $k\t $an->{e} \t\t ".gpty($v);
-
-        if ($an->{e} >= 1 && ref $an->{i}) {
-            my $cu = $G->du($an);
-            while (my ($ku, $vu) = each %$cu) {
-                my $nk = $k.$ku;
-                next if grep { $_->($_, $an, $cu) } @{$s->{notZ}||[]};
-                $c->{$nk} = $vu;
-            }
-        }
-        elsif ($an->{e} > 0 && $an->{e} < 1 && $mustb && !$mustb->{$K}) {
-            delete $c->{$rk};
-        }
-    }
-
-
-    $c
-}
-
-sub dus {
-    my $G = shift;
-    my $h = {
-      ARRAY => {
-        it => sub {
-          my $h = shift;
-          my $i = 0;
-          map { { k => "[".$i++, v => $_ } } @$h
-        },
-        oh => 0,
-      },
-      HASH => {
-        it => sub {
-          my $h = shift;
-          map { { K=>$_, k=>"{".$_, v=>$h->{$_} } } sort keys %$h
-        },
-      },
-    };
-    my $an = sub {
-        my $k = shift;
-        my $i = $h->{$k} ||= {it => $h->{HASH}->{it}};
-        %$i = (%$i, @_);
-    };
-    $an->(qw'A oh 0.2');
-    $an->(qw'Ghost oh 0.8');
-    $an->(qw'G oh 0.8');
-    $an->(qw'W oh 0.8 mustb','id,hash,file,G');
-    $h
 }
 
 sub F_delta {
