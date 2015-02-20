3d019ae29a5a7e248320a585716d76eeed7b5dfa
styleshed <nostrasteve@gmail.com>
2015-02-21 11:40:43 +1300

    on the leve

--- l/S.pm
+++ l/S.pm
@@ -135,7 +135,7 @@ __DATA__
           var tag = $(ev.target);
           d.name = tag.attr('name');
           //d.value = a.valblag(d.tag); // drapes
-          while (!tag.attr('id')) {
+          while (tag && !tag.attr('id')) {
                 tag = tag.parent();
           }
           d.id = tag.attr('id');
