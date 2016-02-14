c9f4810ba4f31e91dfe7bcc44ac74af14461686f
s <nostrasteve@gmail.com>
2016-02-15 01:41:14 +1300

    waffle, non order

--- n/W/Medial.c
+++ n/W/Medial.c
@@ -1,6 +1,1 @@
-/art/ritimco.mp4
-/art/clock.mp4
-/art/A Wellington Gondola Ride.mp4
-/art/Alfred North Whitehead Waltz.mp4
-/art/night goat.mp4
-/art/medlos.mp4
+https://www.youtube.com/embed/I5I1AEQ4M2k?list=PLjMNWea6sX7LnV8Jexljx7RufkvIgqj2G
--- n/W/Musectio.c
+++ n/W/Musectio.c
@@ -1,3 +1,1 @@
-/art/Fela Kuti - Water no get enemy.mp4
-/art/Odetta - Sail away, ladies.mp4
 https://www.youtube.com/embed/AFt2Bo4ts7Y?list=PLsBBzycD1EfjQTtydFoo187qyVoNfE7IX
